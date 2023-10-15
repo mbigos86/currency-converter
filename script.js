0b7e01a006cf3f7490b5e263d0c09022c34ded2f
Mariusz Bigos <mbrolety@gmail.com>
2023-10-15 22:19:20 +0200

    add resetResult

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
+{
 const calculateResult = (amount, currency) => {
-  const moneyElement = document.querySelector(".js-cash");
 
   const rateUSD = 4.118;
   const rateEUR = 4.473;
@@ -8,18 +8,23 @@ const calculateResult = (amount, currency) => {
   switch (currency) {
     case "USD":
       return amount / rateUSD;
-      moneyElement.innerText = "USD";
 
     case "EUR":
       return amount / rateEUR;
-      moneyElement.innerText = "EURO";
   }
 };
 
 const showResult = (amount, result, currency) => {
   const resultElement = document.querySelector(".js-result");
   resultElement.innerText = `${amount} PLN to  ${result.toFixed(2)} ${currency}`;
-}
+};
+
+const resetResult = () => {
+  const formElement = document.querySelector(".js-form");
+  const resultElement = document.querySelector(".js-result");
+  formElement.reset();
+  resultElement.innerText = "";
+};
 
 const onFormSubmit = (event) => {
   event.preventDefault();
@@ -28,21 +33,28 @@ const onFormSubmit = (event) => {
   const amountElement = document.querySelector(".js-amount");
   const currencyElement = document.querySelector(".js-currency");
 
-  
+
 
   const amount = amountElement.value
   const currency = currencyElement.value
 
   const result = calculateResult(amount, currency);
 
-showResult (amount, result, currency);
+  showResult(amount, result, currency);
 }
+
 
 const init = () => {
   const formElement = document.querySelector(".js-form");
-  
+  const resetButton = document.querySelector(".js-resetButton");
+
   formElement.addEventListener("submit", onFormSubmit);
+
+  resetButton.addEventListener("click", resetResult);
+
 };
 
 init();
+
+}
 
